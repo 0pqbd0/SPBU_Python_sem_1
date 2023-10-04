88f132f4e4e0caa49ec83ae4f54d6e82a0f6810a
Artem Mikulskii <st107472@student.spbu.ru>
2023-10-04 11:30:34 +0300

    main

--- src/practices/practice_1/task_2_array_pc.py
+++ src/practices/practice_1/task_2_array_pc.py
@@ -3,7 +3,7 @@ def reorder(array, len_1):
     return array
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     length_1 = int(input("Введите количество элементов в первой части:"))
     array = [int(num) for num in input("Введите вектор: ").split()]
     print(reorder(array, length_1))
