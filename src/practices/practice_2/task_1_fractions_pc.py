34e06b281cd86cb893e1e03322c9861565a574cb
Artem Mikulskii <st107472@student.spbu.ru>
2023-10-04 11:23:46 +0300

    changes practice_2_task_1_with_black

--- src/practices/practice_2/task_1_fractions_pc.py
+++ src/practices/practice_2/task_1_fractions_pc.py
@@ -1,9 +1,9 @@
 def print_fractions(n):
-    for i in range (2, n + 1):
-        print(f'1/{i}')
-        for j in range (2, i + 1):
-            if i%j != 0:
-                print(f'{j}/{i}')
+    for i in range(2, n + 1):
+        print(f"1/{i}")
+        for j in range(2, i + 1):
+            if i % j != 0:
+                print(f"{j}/{i}")
 
 
 if __name__ == "__main__":
